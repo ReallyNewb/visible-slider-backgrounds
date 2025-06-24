d3f5738a0531400054c6f78f75b835cec9ff3784
ReallyNewb <bradenlebron@icloud.com>
2025-06-23 23:27:59 -0500

    ios patch (hope this goes well)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/Slider.hpp>
 
+#include <Geode/utils/cocos.hpp>
+
 using namespace geode::prelude;
 
 class $modify(NewSlider, Slider) {
@@ -8,8 +10,13 @@ public:
 	bool init(CCNode* p0, SEL_MenuHandler p1, char const* p2, char const* p3, char const* p4, char const* p5, float p6) {
 		if (!Slider::init(p0, p1, p2, p3, p4, p5, p6)) return false;
 
-		queueInMainThread([this] {
-			auto main = this->getChildByType<CCSprite>(0);
+		auto failsafe = WeakRef(this);
+
+		queueInMainThread([failsafe] {
+			auto self = failsafe.lock();
+			if (!self) return;
+
+			auto main = self->getChildByType<CCSprite>(0);
 			if (!main) return;
 
 			auto bg = main->getChildByType<CCSprite>(0);
