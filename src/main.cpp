0d3bb852a0870336030b65f7e41e5084c44c3a27
ReallyNewb <bradenlebron@icloud.com>
2025-06-23 22:27:58 -0500

    queue in the main thread

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,15 @@ public:
 	bool init(CCNode* p0, SEL_MenuHandler p1, char const* p2, char const* p3, char const* p4, char const* p5, float p6) {
 		if (!Slider::init(p0, p1, p2, p3, p4, p5, p6)) return false;
 
-		this->scheduleOnce(schedule_selector(NewSlider::onSlider), 0.f);
+		queueInMainThread([this] {
+			auto main = this->getChildByType<CCSprite>(0);
+			if (!main) return;
+
+			auto bg = main->getChildByType<CCSprite>(0);
+			if (!bg) return;
+
+			bg->setVisible(true);
+		});
 		return true;
-	}
-
-	void onSlider(float) {
-		auto main = this->getChildByType<CCSprite>(0);
-		if (!main) return;
-
-		auto bg = main->getChildByType<CCSprite>(0);
-		if (!bg) return;
-
-		bg->setVisible(true);
 	}
 };
